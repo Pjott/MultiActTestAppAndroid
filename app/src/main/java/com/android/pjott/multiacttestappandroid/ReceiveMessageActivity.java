9ff8b04eaaa6772306412d63b1196dcd112d1e2c
Pjott <piotr.baa@gmail.com>
2018-02-04 18:46:01 +0100

    Receiving intent to second Activity and update layout

--- app/src/main/java/com/android/pjott/multiacttestappandroid/ReceiveMessageActivity.java
+++ app/src/main/java/com/android/pjott/multiacttestappandroid/ReceiveMessageActivity.java
@@ -1,13 +1,21 @@
 package com.android.pjott.multiacttestappandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends Activity {
+
+    public static final String PASSED_MESSAGE = "init";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageFromIntent = intent.getStringExtra(PASSED_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.message);
+        messageView.setText(messageFromIntent);
     }
 }
